fa1371f3d0fe2eb15799434a30b66a5555f9e499
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-05-21 13:47:51 -0400

    Added css styles

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const inputText = document.getElementById("inputText");
 const mirrorText = document.getElementById("mirrorText");
 
 inputText.addEventListener("input", () => {
-    mirrorText.textContent = inputText.value; 
+    mirrorText.textContent = inputText.value
 
 })
